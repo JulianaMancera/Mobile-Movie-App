3d8becb6c3022f292b8e6985cb1e8f35f7cb4178
JulianaMancera <mancerajuliana98@gmail.com>
2025-09-07 22:47:27 +0800

    working

--- Mobile-Movie-App/services/api.ts
+++ Mobile-Movie-App/services/api.ts
@@ -1,13 +1,15 @@
 export const TMDB_CONFIG = {
-    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_URL: "https://api.themoviedb.org/3",
     API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
     headers: {
-        accept: 'application/json',
+        accept: "application/json",
         Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`
     }
 }
 
-export const fetchPopularMovies = async ({ query }: { query: string}) => {
+export const fetchMovies = async ({
+     query,
+     }: { query: string}) => {
     const endpoint = query
     ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
     : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
@@ -23,7 +25,6 @@ export const fetchPopularMovies = async ({ query }: { query: string}) => {
     }
 
     const data = await response.json();
-
     return data.results;
 }
 // discover/movie
